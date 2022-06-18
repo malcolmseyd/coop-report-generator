74bf1bf63db124a88ac100b56f28005c5526d3b4
Dominique Charlebois <domo.charlebois@gmail.com>
2022-06-18 15:17:34 -0700

    fetch request

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,43 @@
-import Fastify from 'fastify'
-import Handlebars from 'handlebars'
-import axios from "axios"
+import Fastify from "fastify";
+import Handlebars from "handlebars";
+import axios from "axios";
 const fastify = Fastify({
-	logger: true
-})
-import fs from "fs/promises"
+  logger: true,
+});
+import fs from "fs/promises";
 
-fastify.get('/', async (request, reply) => {
-	return { hello: 'world' }
-})
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
 
 fastify.get("/kanye", async (request, reply) => {
-	const resp = await axios.get("https://api.kanye.rest/")
-	return resp.data
-})
+  const resp = await axios.get("https://api.kanye.rest/");
+  return resp.data;
+});
 
 fastify.post("/echo", async (request, reply) => {
-	return request.body
-})
-
+  return request.body;
+});
 
 fastify.post("/template", async (request, reply) => {
-	const t = Handlebars.compile("The time is {{time}}")
-	return { result: t({ time: new Date().toISOString() }) }
-})
+  const t = Handlebars.compile("The time is {{time}}");
+  return { result: t({ time: new Date().toISOString() }) };
+});
 
 fastify.get("/hosts", async (request, reply) => {
-	const a = await fs.readFile("/etc/hosts")
-	return a.toString()
-})
+  const a = await fs.readFile("/etc/hosts");
+  return a.toString();
+});
 
 /**
  * Run the server!
  */
 const start = async () => {
-	try {
-		await fastify.listen({ port: 3000 })
-	} catch (err) {
-		fastify.log.error(err)
-		process.exit(1)
-	}
-}
-start()
+  try {
+    await fastify.listen({ port: 8080 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
