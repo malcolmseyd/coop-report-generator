4183274a5a84f5da04e964852b9aa46eb2193418
Dominique Charlebois <domo.charlebois@gmail.com>
2022-06-18 10:16:05 -0700

    add tailwind and prettier

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
