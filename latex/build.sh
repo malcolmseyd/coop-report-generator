7d7eeb173f7870e607441727de29f3f5e09367d2
Malcolm Seyd <malcolmseyd@gmail.com>
2022-06-18 12:07:10 -0700

    rename to engr for clarity

--- latex/build.sh
+++ latex/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pdflatex -output-directory letter letter/uvic-csc-letter-of-transmittal.tex
-pdflatex uvic-csc-work-term-report.tex
+pdflatex -output-directory letter letter/uvic-engr-letter-of-transmittal.tex
+pdflatex uvic-engr-work-term-report.tex
