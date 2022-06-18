5fe2e6e756460ced957681d00f08924d0925b101
Malcolm Seyd <malcolmseyd@gmail.com>
2022-06-18 10:14:50 -0700

    axios works

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,27 @@
 import Fastify from 'fastify'
+import axios from "axios"
 const fastify = Fastify({
-  logger: true
+	logger: true
 })
 
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+	return { hello: 'world' }
+})
+
+fastify.get("/kanye", async (request, reply) => {
+	const resp = await axios.get("https://api.kanye.rest/")
+	return resp.data
 })
 
 /**
  * Run the server!
  */
 const start = async () => {
-  try {
-    await fastify.listen({ port: 3000 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+	try {
+		await fastify.listen({ port: 3000 })
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
 }
 start()
