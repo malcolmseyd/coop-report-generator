fae0d498c86c5bd2bdb4b3df145a3d1a27b1cf8b
Malcolm Seyd <malcolmseyd@gmail.com>
2022-06-18 13:28:41 -0700

    title page and letter of transmittal are templated

---
+++ backend/src/schema.js
@@ -0,0 +1,33 @@
+export default
+{
+	title: "Is Stadia Worth Building?",
+	term: {
+		season: "Fall",
+		year: "2022",
+		workTermNumber: "3"
+	},
+	company: {
+		name: "Google",
+		position: "Developer",
+		location: "Victoria, BC, Canada",
+	},
+	student: {
+		name: "Rui Ueyama",
+		number: "V00389870",
+		discipline: "Software Engineering",
+		email: "rui314@uvic.ca",
+		address: "1392 Allegro Royal Lane",
+		city: "Edmonton",
+		province: "Alberta",
+		zip: "A0M 1S2"
+	},
+	dueDate: "April 24th, 2022",
+	marker: {
+		name: "Kevin Hall",
+		address: "1392 Allegro Royal Lane",
+		city: "Victoria",
+		province: "BC",
+		zip: "H0H 0H0"
+	},
+	transmittal: "Please accept the acccompan...",
+}
